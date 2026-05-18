acd450cfe4499609a8d1c5d7e415afcd05e84c9c
AudrenMeteryDrouin <138331587+AmadeusTdev@users.noreply.github.com>
2026-05-18 09:36:09 +0000

    exercice4 fait

---
+++ src/main/java/fr/univ_amu/iut/exercice4/Chat.java
@@ -0,0 +1,11 @@
+package fr.univ_amu.iut.exercice4;
+
+public class Chat extends Animal {
+  public Chat(String nom) {
+    super(nom);
+  }
+
+  public String faireDuBruit() {
+    return "Miaou !";
+  }
+}
---
+++ src/main/java/fr/univ_amu/iut/exercice4/Vache.java
@@ -0,0 +1,11 @@
+package fr.univ_amu.iut.exercice4;
+
+public class Vache extends Animal {
+  public Vache(String nom) {
+    super(nom);
+  }
+
+  public String faireDuBruit() {
+    return "Meuh !";
+  }
+}

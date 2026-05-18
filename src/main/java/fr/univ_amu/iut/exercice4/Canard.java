1d7656fcf99d77da6bf82303f313dae371da0e72
AudrenMeteryDrouin <138331587+AmadeusTdev@users.noreply.github.com>
2026-05-18 09:36:09 +0000

    exercice4 fait

---
+++ src/main/java/fr/univ_amu/iut/exercice4/Canard.java
@@ -0,0 +1,11 @@
+package fr.univ_amu.iut.exercice4;
+
+public class Canard extends Animal {
+  public Canard(String nom) {
+    super(nom);
+  }
+
+  public String faireDuBruit() {
+    return "Coin coin !";
+  }
+}
